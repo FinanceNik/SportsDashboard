df52a3c63f027b7393f43532622838f3374183c7
thequant <thequant@pop-os.localdomain>
2022-12-16 17:12:55 +0100

    commenting on colours

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ import warnings
 warnings.filterwarnings('ignore')
 warnings.simplefilter("ignore", UserWarning)
 
-app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.BOOTSTRAP], url_base_pathname='/')
+app = dash.Dash(__name__, suppress_callback_exceptions=True,
+                external_stylesheets=[dbc.themes.BOOTSTRAP],
+                url_base_pathname='/')
 
 
 sidebar = html.Div(
