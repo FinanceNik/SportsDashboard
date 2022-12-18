ff91a2030366e4e0eb2794d22f5e1bb54c69a32b
thequant <thequant@pop-os.localdomain>
2022-12-18 16:33:23 +0100

    completed data cleaning process and instatianted the required functions

--- DataHandler.py
+++ DataHandler.py
@@ -1,23 +1,28 @@
-import numpy as np
 import pandas as pd
+from datetime import datetime  # for getting the current year
+
+currentYear = int(datetime.today().strftime('%Y'))
 
 
-def dataCleaner(activityType):  # --> string: Can be [Ride, Run, Walk, Hike]
+def dataCleaner(activityType, startYear):  # --> string: Can be [Ride, Run, Walk, Hike]
     df = pd.read_csv('activities.csv')
-    df.insert(1, 'year', '')
-    df.insert(1, 'month', '')
-    print((df['Activity Date'][3]))
-    # Convert the string object into a Datetime object
-    # Then, extract year and month and fill in new columns with that.
-    return df.loc[df['Activity Type'] == activityType]
+    df['Activity Date'] = pd.to_datetime(df['Activity Date'])
+
+    df.insert(1, "year", "")
+    df['year'] = df['Activity Date'].dt.year
+
+    df.insert(1, "month", "")
+    df['month'] = df['Activity Date'].dt.month
+
+    if activityType == "all":
+        return df.loc[df['year'] == startYear]
+    else:
+        df = df.loc[df['Activity Type'] == activityType]
+        return df.loc[df['year'] == startYear]
 
 
-dataCleaner('Ride')
+def uniqueActivityType():
+    df = dataCleaner("all", currentYear)
+    activityTypes = df["Activity Type"].unique()
+    return activityTypes
 
-
-def mainKpiFiller(activityType):
-    df = dataCleaner(activityType=activityType)
-    # totalActivities:
-    # totalTime:
-    # totalDistance:
-    # totalElevation:
