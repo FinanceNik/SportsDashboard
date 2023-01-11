61ed743c37c4f2139e0fa50518cea089c3807309
thequant <thequant@pop-os.localdomain>
2023-01-11 16:37:38 +0100

    minor adjustments

--- main.py
+++ main.py
@@ -11,9 +11,6 @@ import pandas as pd
 import warnings
 import page_about
 
-warnings.filterwarnings('ignore')
-warnings.simplefilter("ignore", UserWarning)
-
 app = dash.Dash(__name__, suppress_callback_exceptions=True,
                 external_stylesheets=[dbc.themes.BOOTSTRAP],
                 url_base_pathname='/')
